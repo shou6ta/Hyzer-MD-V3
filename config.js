76a398744cd5cd52456e66712c0d2a737bc87875
shou6ta <109680356+shou6ta@users.noreply.github.com>
2022-07-24 21:37:23 +0700

    Update config.js

--- config.js
+++ config.js
@@ -19,8 +19,8 @@ global.web = 'https://github.com/AzamXd' //ubah jadi website lu, bisa link ig, l
 global.price1 = '1 bulan = 10.000\n2 Minggu 7.000\n1 Minggu 5.000'
 
 //=======『 Tampilan Dan Lainnya 』============//
-global.fotonya1 = 'https://telegra.ph/file/e4a2f4339da8a32ad20a1.jpg' //ganti jadi foto bot mu
-global.fotonya2 = 'https://telegra.ph/file/8c471d45b6cedad43cf84.jpg' //ini juga ganti 
+global.fotonya1 = 'https://telegra.ph/6281389368688081389368688-07-24' //ganti jadi foto bot mu
+global.fotonya2 = 'https://telegra.ph/6281389368688081389368688-07-24' //ini juga ganti 
 global.lolkey = 'Papah-Chan' //biar mudah ngegantinya semisal apikeynya expired:v
 global.zenzkey = 'BagasPrdn' //ganti jadi apikey lu kalau expired
 global.wm = 'Shou6ta'
